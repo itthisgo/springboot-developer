ed756ca89c12fa3b48e13807c79ccb8535ab0463
itthisgo@gmail.com <itthisgo@gmail.com>
2024-09-27 09:37:28 +0900

    validation with unused javafaker

--- src/main/java/com/ithotgi/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/com/ithotgi/springbootdeveloper/controller/BlogApiController.java
@@ -8,7 +8,9 @@ import com.ithotgi.springbootdeveloper.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
@@ -21,7 +23,7 @@ public class BlogApiController {
     private final BlogService blogService;
 
     @PostMapping("/api/articles")
-    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request, Principal principal){
+    public ResponseEntity<Article> addArticle(@RequestBody @Validated AddArticleRequest request, Principal principal){
         Article savedArticle = blogService.save(request, principal.getName());
 
         return ResponseEntity.status(HttpStatus.CREATED)
--- src/main/java/com/ithotgi/springbootdeveloper/dto/AddArticleRequest.java
+++ src/main/java/com/ithotgi/springbootdeveloper/dto/AddArticleRequest.java
@@ -1,6 +1,8 @@
 package com.ithotgi.springbootdeveloper.dto;
 
 import com.ithotgi.springbootdeveloper.domain.Article;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +11,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Getter
 public class AddArticleRequest {
+
+    @NotNull
+    @Size(min = 1, max = 10)
     private String title;
+
+    @NotNull
     private String content;
 
     public Article toEntity(String author){
